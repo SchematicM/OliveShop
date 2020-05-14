20b34f9f843c2296365c7e3f717d0f0392d6405e
Martina Skrypnyk <37904208+SchematicM@users.noreply.github.com>
2020-05-15 02:23:29 +0300

    Added Footer and Copyright block with active links on DEV's instagram

--- M-Dev-Store/index.php
+++ M-Dev-Store/index.php
@@ -629,6 +629,12 @@
        </div><!-- row Finish -->
        
    </div><!-- container Finish -->
+   
+   <?php 
+    
+    include("includes/footer.php");
+    
+    ?>
     
     <script src="js/jquery-331.min.js"></script>
     <script src="js/bootstrap-337.min.js"></script>
