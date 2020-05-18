ddc34f85176a096225c8ea9a58be0b802c2dd1c9
SolomiyaRybai <55078917+SolomiyaRybai@users.noreply.github.com>
2020-05-18 23:34:59 +0300

    Made Shop Page Dynamically

--- M-Dev-Store/functions/functions.php
+++ M-Dev-Store/functions/functions.php
@@ -2,6 +2,8 @@
 
 $db = mysqli_connect("localhost","root","","ecom_store");
 
+/// begin getPro functions ///
+
 function getPro(){
     
     global $db;
@@ -77,5 +79,73 @@ function getPro(){
     }
     
 }
+
+/// finish getPro functions ///
+
+/// begin getPCats functions ///
+
+function getPCats(){
+    
+    global $db;
+    
+    $get_p_cats = "select * from product_categories";
+    
+    $run_p_cats = mysqli_query($db,$get_p_cats);
+    
+    while($row_p_cats=mysqli_fetch_array($run_p_cats)){
+        
+        $p_cat_id = $row_p_cats['p_cat_id'];
+        
+        $p_cat_title = $row_p_cats['p_cat_title'];
+        
+        echo "
+        
+            <li>
+            
+                <a href='shop.php?p_cat=$p_cat_id'> $p_cat_title </a>
+            
+            </li>
+        
+        ";
+        
+    }
+    
+}
+    
+/// finish getPCats functions ///
+
+/// begin getCats functions ///
+
+function getCats(){
+    
+    global $db;
+    
+    $get_cats = "select * from categories";
+    
+    $run_cats = mysqli_query($db,$get_cats);
+    
+    while($row_cats=mysqli_fetch_array($run_cats)){
+        
+        $cat_id = $row_cats['cat_id'];
+        
+        $cat_title = $row_cats['cat_title'];
+        
+        echo "
+        
+            <li>
+            
+                <a href='shop.php?cat=$cat_id'> $cat_title </a>
+            
+            </li>
+        
+        ";
+        
+    }
+    
+}
+    
+/// finish getCats functions ///
+
+
 
 ?>
