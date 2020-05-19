50346cf76d91563a7c1707feef6e85f841d60e23
Martina Skrypnyk <37904208+SchematicM@users.noreply.github.com>
2020-05-20 00:47:07 +0300

    Created LOGIN PAGE  and made payment opinions for authorised users

--- M-Dev-Store/customer/includes/header.php
+++ M-Dev-Store/customer/includes/header.php
@@ -1,11 +1,49 @@
 <?php 
 
 session_start();
+
 include("includes/db.php");
 include("functions/functions.php");
 
 ?>
 
+<?php 
+
+if(isset($_GET['pro_id'])){
+    
+    $product_id = $_GET['pro_id'];
+    
+    $get_product = "select * from products where product_id='$product_id'";
+    
+    $run_product = mysqli_query($con,$get_product);
+    
+    $row_product = mysqli_fetch_array($run_product);
+    
+    $p_cat_id = $row_product['p_cat_id'];
+    
+    $pro_title = $row_product['product_title'];
+    
+    $pro_price = $row_product['product_price'];
+    
+    $pro_desc = $row_product['product_desc'];
+    
+    $pro_img1 = $row_product['product_img1'];
+    
+    $pro_img2 = $row_product['product_img2'];
+    
+    $pro_img3 = $row_product['product_img3'];
+    
+    $get_p_cat = "select * from product_categories where p_cat_id='$p_cat_id'";
+    
+    $run_p_cat = mysqli_query($con,$get_p_cat);
+    
+    $row_p_cat = mysqli_fetch_array($run_p_cat);
+    
+    $p_cat_title = $row_p_cat['p_cat_title'];
+    
+}
+
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -66,7 +104,7 @@ include("functions/functions.php");
                            
                            if(!isset($_SESSION['customer_email'])){
                        
-                                echo "<a href='checkout.php'> Login </a>";
+                                echo "<a href='../checkout.php'> Login </a>";
 
                                }else{
 
@@ -144,7 +182,7 @@ include("functions/functions.php");
                    
                </div><!-- padding-nav Finish -->
                
-               <a href="cart.php" class="btn navbar-btn btn-primary right"><!-- btn navbar-btn btn-primary Begin -->
+               <a href="../cart.php" class="btn navbar-btn btn-primary right"><!-- btn navbar-btn btn-primary Begin -->
                    
                    <i class="fa fa-shopping-cart"></i>
                    
