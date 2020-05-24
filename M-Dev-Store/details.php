28537a47d91fec9f36f82eee9403ff62422fcff7
Martina Skrypnyk <37904208+SchematicM@users.noreply.github.com>
2020-05-24 04:37:27 +0300

    Made sidebar dynamic. Added function of search categories by letters, hiding sidebar.

--- M-Dev-Store/details.php
+++ M-Dev-Store/details.php
@@ -1,9 +1,273 @@
 <?php 
 
-    $active='Cart';
-    include("includes/header.php");
+session_start();
+
+$active='Cart';
+
+include("includes/db.php");
+include("functions/functions.php");
 
 ?>
+
+<?php 
+
+if(isset($_GET['pro_id'])){
+    
+    $product_id = $_GET['pro_id'];
+    
+    $get_product = "select * from products where product_id='$product_id'";
+    
+    $run_product = mysqli_query($con,$get_product);
+    
+    $row_products = mysqli_fetch_array($run_product);
+    
+    $p_cat_id = $row_products['p_cat_id'];
+    
+    $pro_title = $row_products['product_title'];
+    
+    $pro_price = $row_products['product_price'];
+
+    $pro_sale_price = $row_products['product_sale'];
+    
+    $pro_desc = $row_products['product_desc'];
+    
+    $pro_img1 = $row_products['product_img1'];
+    
+    $pro_img2 = $row_products['product_img2'];
+    
+    $pro_img3 = $row_products['product_img3'];
+        
+    $pro_label = $row_products['product_label'];
+
+    if($pro_label == ""){
+
+    }else{
+
+        $product_label = "
+        
+            <a href='#' class='label $pro_label'>
+            
+                <div class='theLabel'> $pro_label </div>
+                <div class='labelBackground'>  </div>
+            
+            </a>
+        
+        ";
+
+    }
+    
+    $get_p_cat = "select * from product_categories where p_cat_id='$p_cat_id'";
+    
+    $run_p_cat = mysqli_query($con,$get_p_cat);
+    
+    $row_p_cat = mysqli_fetch_array($run_p_cat);
+    
+    $p_cat_title = $row_p_cat['p_cat_title'];
+    
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>M-Dev Store</title>
+    <link rel="stylesheet" href="styles/bootstrap-337.min.css">
+    <link rel="stylesheet" href="font-awsome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styles/style.css">
+</head>
+<body>
+   
+   <div id="top"><!-- Top Begin -->
+       
+       <div class="container"><!-- container Begin -->
+           
+           <div class="col-md-6 offer"><!-- col-md-6 offer Begin -->
+               
+               <a href="#" class="btn btn-success btn-sm">
+                   
+                   <?php 
+                   
+                   if(!isset($_SESSION['customer_email'])){
+                       
+                       echo "Welcome: Guest";
+                       
+                   }else{
+                       
+                       echo "Welcome: " . $_SESSION['customer_email'] . "";
+                       
+                   }
+                   
+                   ?>
+                   
+               </a>
+               <a href="checkout.php"><?php items(); ?> Items In Your Cart | Total Price: <?php total_price(); ?> </a>
+               
+           </div><!-- col-md-6 offer Finish -->
+           
+           <div class="col-md-6"><!-- col-md-6 Begin -->
+               
+               <ul class="menu"><!-- cmenu Begin -->
+                   
+                   <li>
+                       <a href="customer_register.php">Register</a>
+                   </li>
+                   <li>
+                       <a href="checkout.php">My Account</a>
+                   </li>
+                   <li>
+                       <a href="cart.php">Go To Cart</a>
+                   </li>
+                   <li>
+                       <a href="checkout.php">
+                           
+                           <?php 
+                           
+                           if(!isset($_SESSION['customer_email'])){
+                       
+                                echo "<a href='checkout.php'> Login </a>";
+
+                               }else{
+
+                                echo " <a href='logout.php'> Log Out </a> ";
+
+                               }
+                           
+                           ?>
+                           
+                       </a>
+                   </li>
+                   
+               </ul><!-- menu Finish -->
+               
+           </div><!-- col-md-6 Finish -->
+           
+       </div><!-- container Finish -->
+       
+   </div><!-- Top Finish -->
+   
+   <div id="navbar" class="navbar navbar-default"><!-- navbar navbar-default Begin -->
+       
+       <div class="container"><!-- container Begin -->
+           
+           <div class="navbar-header"><!-- navbar-header Begin -->
+               
+               <a href="index.php" class="navbar-brand home"><!-- navbar-brand home Begin -->
+                   
+                   <img src="images/ecom-store-logo.png" alt="M-dev-Store Logo" class="hidden-xs">
+                   <img src="images/ecom-store-logo-mobile.png" alt="M-dev-Store Logo Mobile" class="visible-xs">
+                   
+               </a><!-- navbar-brand home Finish -->
+               
+               <button class="navbar-toggle" data-toggle="collapse" data-target="#navigation">
+                   
+                   <span class="sr-only">Toggle Navigation</span>
+                   
+                   <i class="fa fa-align-justify"></i>
+                   
+               </button>
+               
+               <button class="navbar-toggle" data-toggle="collapse" data-target="#search">
+                   
+                   <span class="sr-only">Toggle Search</span>
+                   
+                   <i class="fa fa-search"></i>
+                   
+               </button>
+               
+           </div><!-- navbar-header Finish -->
+           
+           <div class="navbar-collapse collapse" id="navigation"><!-- navbar-collapse collapse Begin -->
+               
+               <div class="padding-nav"><!-- padding-nav Begin -->
+                   
+                   <ul class="nav navbar-nav left"><!-- nav navbar-nav left Begin -->
+                       
+                       <li class="<?php if($active=='Home') echo"active"; ?>">
+                           <a href="index.php">Home</a>
+                       </li>
+                       <li class="<?php if($active=='Shop') echo"active"; ?>">
+                           <a href="shop.php">Shop</a>
+                       </li>
+                       <li class="<?php if($active=='Account') echo"active"; ?>">
+                           
+                           <?php 
+                           
+                           if(!isset($_SESSION['customer_email'])){
+                               
+                               echo"<a href='checkout.php'>My Account</a>";
+                               
+                           }else{
+                               
+                              echo"<a href='customer/my_account.php?my_orders'>My Account</a>"; 
+                               
+                           }
+                           
+                           ?>
+                           
+                       </li>
+                       <li class="<?php if($active=='Cart') echo"active"; ?>">
+                           <a href="cart.php">Shopping Cart</a>
+                       </li>
+                       <li class="<?php if($active=='Contact') echo"active"; ?>">
+                           <a href="contact.php">Contact Us</a>
+                       </li>
+                       
+                   </ul><!-- nav navbar-nav left Finish -->
+                   
+               </div><!-- padding-nav Finish -->
+               
+               <a href="cart.php" class="btn navbar-btn btn-primary right"><!-- btn navbar-btn btn-primary Begin -->
+                   
+                   <i class="fa fa-shopping-cart"></i>
+                   
+                   <span><?php items(); ?> Items In Your Cart</span>
+                   
+               </a><!-- btn navbar-btn btn-primary Finish -->
+               
+               <div class="navbar-collapse collapse right"><!-- navbar-collapse collapse right Begin -->
+                   
+                   <button class="btn btn-primary navbar-btn" type="button" data-toggle="collapse" data-target="#search"><!-- btn btn-primary navbar-btn Begin -->
+                       
+                       <span class="sr-only">Toggle Search</span>
+                       
+                       <i class="fa fa-search"></i>
+                       
+                   </button><!-- btn btn-primary navbar-btn Finish -->
+                   
+               </div><!-- navbar-collapse collapse right Finish -->
+               
+               <div class="collapse clearfix" id="search"><!-- collapse clearfix Begin -->
+                   
+                   <form method="get" action="results.php" class="navbar-form"><!-- navbar-form Begin -->
+                       
+                       <div class="input-group"><!-- input-group Begin -->
+                           
+                           <input type="text" class="form-control" placeholder="Search" name="user_query" required>
+                           
+                           <span class="input-group-btn"><!-- input-group-btn Begin -->
+                           
+                           <button type="submit" name="search" value="Search" class="btn btn-primary"><!-- btn btn-primary Begin -->
+                               
+                               <i class="fa fa-search"></i>
+                               
+                           </button><!-- btn btn-primary Finish -->
+                           
+                           </span><!-- input-group-btn Finish -->
+                           
+                       </div><!-- input-group Finish -->
+                       
+                   </form><!-- navbar-form Finish -->
+                   
+               </div><!-- collapse clearfix Finish -->
+               
+           </div><!-- navbar-collapse collapse Finish -->
+           
+       </div><!-- container Finish -->
+       
+   </div><!-- navbar navbar-default Finish -->
    
    <div id="content"><!-- #content Begin -->
        <div class="container"><!-- container Begin -->
@@ -25,17 +289,7 @@
                
            </div><!-- col-md-12 Finish -->
            
-           <div class="col-md-3"><!-- col-md-3 Begin -->
-   
-   <?php 
-    
-    include("includes/sidebar.php");
-    
-    ?>
-               
-           </div><!-- col-md-3 Finish -->
-           
-           <div class="col-md-9"><!-- col-md-9 Begin -->
+           <div class="col-md-12"><!-- col-md-12 Begin -->
                <div id="productMain" class="row"><!-- row Begin -->
                    <div class="col-sm-6"><!-- col-sm-6 Begin -->
                        <div id="mainImage"><!-- #mainImage Begin -->
@@ -65,11 +319,14 @@
                                
                                <a href="#myCarousel" class="right carousel-control" data-slide="next"><!-- right carousel-control Begin -->
                                    <span class="glyphicon glyphicon-chevron-right"></span>
-                                   <span class="sr-only">Previous</span>
+                                   <span class="sr-only">Next</span>
                                </a><!-- right carousel-control Finish -->
                                
                            </div><!-- carousel slide Finish -->
                        </div><!-- mainImage Finish -->
+
+                           <?php echo $product_label; ?>
+
                    </div><!-- col-sm-6 Finish -->
                    
                    <div class="col-sm-6"><!-- col-sm-6 Begin -->
@@ -112,7 +369,37 @@
                                    </div><!-- col-md-7 Finish -->
                                </div><!-- form-group Finish -->
                                
-                               <p class="price">$ <?php echo $pro_price; ?></p>
+                               <?php 
+
+                                    if($pro_label == "sale"){
+
+                                        echo "
+
+                                            <p class='price'>
+
+                                            PRICE: <del> $$pro_price</del><br/>
+
+                                            SALE: $    $pro_sale_price
+
+                                            </p>
+
+                                        ";
+
+                                    }else{
+
+                                        echo "
+
+                                            <p class='price'>
+
+                                            PRICE: $ $pro_price
+
+                                            </p>
+
+                                        ";
+
+                                    }
+                               
+                               ?>
                                
                                <p class="text-center buttons"><button class="btn btn-primary i fa fa-shopping-cart"> Add to cart</button></p>
                                
@@ -184,39 +471,120 @@
                    
                    while($row_products=mysqli_fetch_array($run_products)){
                        
-                       $pro_id = $row_products['product_id'];
-                       
-                       $pro_title = $row_products['product_title'];
-                       
-                       $pro_img1 = $row_products['product_img1'];
-                       
-                       $pro_price = $row_products['product_price'];
-                       
-                       echo "
-                       
-                        <div class='col-md-3 col-sm-6 center-responsive'>
+                    $pro_id = $row_products['product_id'];
+        
+                    $pro_title = $row_products['product_title'];
+                    
+                    $pro_price = $row_products['product_price'];
+            
+                    $pro_sale_price = $row_products['product_sale'];
+                    
+                    $pro_img1 = $row_products['product_img1'];
+                    
+                    $pro_label = $row_products['product_label'];
+                    
+                    $manufacturer_id = $row_products['manufacturer_id'];
+            
+                    $get_manufacturer = "select * from manufacturers where manufacturer_id='$manufacturer_id'";
+            
+                    $run_manufacturer = mysqli_query($db,$get_manufacturer);
+            
+                    $row_manufacturer = mysqli_fetch_array($run_manufacturer);
+            
+                    $manufacturer_title = $row_manufacturer['manufacturer_title'];
+            
+                    if($pro_label == "sale"){
+            
+                        $product_price = " <del> $ $pro_price </del> ";
+            
+                        $product_sale_price = "/ $ $pro_sale_price ";
+            
+                    }else{
+            
+                        $product_price = "  $ $pro_price  ";
+            
+                        $product_sale_price = "";
+            
+                    }
+            
+                    if($pro_label == ""){
+            
+                    }else{
+            
+                        $product_label = "
                         
-                            <div class='product same-height'>
+                            <a href='#' class='label $pro_label'>
                             
-                                <a href='details.php?pro_id=$pro_id'>
+                                <div class='theLabel'> $pro_label </div>
+                                <div class='labelBackground'>  </div>
+                            
+                            </a>
+                        
+                        ";
+            
+                    }
+                    
+                    echo "
+                    
+                    <div class='col-md-3 col-sm-6 center-responsive'>
+                    
+                        <div class='product'>
+                        
+                            <a href='details.php?pro_id=$pro_id'>
+                            
+                                <img class='img-responsive' src='admin_area/product_images/$pro_img1'>
+                            
+                            </a>
+                            
+                            <div class='text'>
+            
+                            <center>
+                            
+                                <p class='btn btn-primary'> $manufacturer_title </p>
+                            
+                            </center>
+                            
+                                <h3>
+                        
+                                    <a href='details.php?pro_id=$pro_id'>
+            
+                                        $pro_title
+            
+                                    </a>
                                 
-                                    <img class='img-responsive' src='admin_area/product_images/$pro_img1'>
+                                </h3>
                                 
-                                </a>
+                                <p class='price'>
                                 
-                                <div class='text'>
+                                $product_price &nbsp;$product_sale_price
                                 
-                                    <h3> <a href='details.php?pro_id=$pro_id'> $pro_title </a> </h3>
-                                    
-                                    <p class='price'> $ $pro_price </p>
+                                </p>
                                 
-                                </div>
+                                <p class='button'>
+                                
+                                    <a class='btn btn-default' href='details.php?pro_id=$pro_id'>
+            
+                                        View Details
+            
+                                    </a>
+                                
+                                    <a class='btn btn-primary' href='details.php?pro_id=$pro_id'>
+            
+                                        <i class='fa fa-shopping-cart'></i> Add to Cart
+            
+                                    </a>
+                                
+                                </p>
                             
                             </div>
+            
+                            $product_label
                         
                         </div>
-                       
-                       ";
+                    
+                    </div>
+                    
+                    ";
                        
                    }
                    
@@ -224,7 +592,7 @@
                    
                </div><!-- #row same-heigh-row Finish -->
                
-           </div><!-- col-md-9 Finish -->
+           </div><!-- col-md-12 Finish -->
            
        </div><!-- container Finish -->
    </div><!-- #content Finish -->
